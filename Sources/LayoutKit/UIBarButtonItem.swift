7c486bffaeb7e4e79974dfb79f46b5e14a9c7fa4
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-08 17:42:47 +0300

    Add UIBarButtonItem create extensions

--- Sources/LayoutKit/UIBarButtonItem.swift
+++ Sources/LayoutKit/UIBarButtonItem.swift
@@ -10,10 +10,100 @@ import UIKit
 public extension UIBarButtonItem {
     
     /**
-     Create UIBarButtonItem
+     Create UIBarButtonItem with system button items.
      */
-    static func create() -> UIBarButtonItem {
-        let barButton = UIBarButtonItem()
+    static func create(buttonItem: SystemItem = .add,
+                       target: Any? = nil,
+                       tintColor: UIColor = .black,
+                       action: Selector? = nil) -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(barButtonSystemItem: buttonItem,
+                                        target: target,
+                                        action: action)
+        barButton.tintColor = tintColor
+        return barButton
+    }
+    
+    /**
+     Create UIBarButtonItem with image.
+     */
+    static func create(image: UIImage? = nil,
+                       style: Style = .done,
+                       target: Any? = nil,
+                       tintColor: UIColor = .black,
+                       action: Selector? = nil) -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(image: image,
+                                        style: style,
+                                        target: target,
+                                        action: action)
+        barButton.tintColor = tintColor
+        return barButton
+    }
+    
+    /**
+     Create UIBarButtonItem with title.
+     */
+    static func create(title: String? = nil,
+                       style: Style = .done,
+                       target: Any? = nil,
+                       tintColor: UIColor = .black,
+                       action: Selector? = nil) -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(title: title,
+                                        style: style,
+                                        target: target,
+                                        action: action)
+        barButton.tintColor = tintColor
+        return barButton
+    }
+    
+    /**
+     Create UIBarButtonItem with image and landspace image.
+     */
+    static func create(image: UIImage? = nil,
+                       landscapeImagePhone: UIImage? = nil,
+                       style: Style = .done,
+                       target: Any? = nil,
+                       tintColor: UIColor = .black,
+                       action: Selector? = nil) -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(image: image,
+                                        landscapeImagePhone: landscapeImagePhone,
+                                        style: style,
+                                        target: target,
+                                        action: action)
+        barButton.tintColor = tintColor
+        return barButton
+    }
+    
+    /**
+     Create UIBarButtonItem with system item.
+     This method is available in iOS 14 and later versions.
+     */
+    @available(iOS 14.0, *)
+    static func create(systemItem: SystemItem = .add,
+                       primaryAction: UIAction? = nil,
+                       menu: UIMenu? = nil,
+                       tintColor: UIColor = .black) -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(systemItem: systemItem,
+                                        primaryAction: primaryAction,
+                                        menu: menu)
+        barButton.tintColor = tintColor
+        return barButton
+    }
+    
+    /**
+     Create UIBarButtonItem with title and image.
+     This method is available in iOS 14 and later versions.
+     */
+    @available(iOS 14.0, *)
+    static func create(title: String? = nil,
+                       image: UIImage? = nil,
+                       tintColor: UIColor = .black,
+                       primaryAction: UIAction? = nil,
+                       menu: UIMenu? = nil) -> UIBarButtonItem {
+        let barButton = UIBarButtonItem(title: title,
+                                        image: image,
+                                        primaryAction: primaryAction,
+                                        menu: menu)
+        barButton.tintColor = tintColor
         return barButton
     }
 }
