82f14dc73f7f56860d478cd16c9e8845ccbf3aea
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-08 16:09:33 +0300

    Create UIActivityIndicatorView create extension

--- Sources/LayoutKit/UIActivityIndicatorView.swift
+++ Sources/LayoutKit/UIActivityIndicatorView.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
+//MARK: - Create UIActivityIndicatorView
 public extension UIActivityIndicatorView {
     
     /**
      Create UIActivityIndicatorView
      */
-    static func create() -> UIActivityIndicatorView {
+    static func create(hidesWhenStopped: Bool,
+                       style: UIActivityIndicatorView.Style? = nil,
+                       color: UIColor = .gray) -> UIActivityIndicatorView {
         let indicator = UIActivityIndicatorView()
+        
+        indicator.hidesWhenStopped = hidesWhenStopped
+        indicator.color = color
+        
+        if #available(iOS 13.0, *), let style = style {
+            indicator.style = style
+        }
+        
         return indicator
     }
 }
