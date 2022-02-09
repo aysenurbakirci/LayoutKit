889996c6512402ef90b2a7f0bf0a4d31ede29636
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-09 14:08:56 +0300

    Add UICollectionViewFlowLayout create extension

--- Sources/CreateView/UICollectionViewFlowLayout.swift
+++ Sources/CreateView/UICollectionViewFlowLayout.swift
@@ -12,8 +12,22 @@ public extension UICollectionViewFlowLayout {
     /**
      Create UICollectionViewFlowLayout
      */
-    static func create() -> UICollectionViewFlowLayout {
+    static func create(itemSize: CGSize = .zero,
+                       headerSize: CGSize = .zero,
+                       footerSize: CGSize = .zero,
+                       scrollDirection: UICollectionView.ScrollDirection = .vertical,
+                       interItemSpacing: CGFloat = .leastNormalMagnitude,
+                       lineSpacing: CGFloat = .leastNormalMagnitude,
+                       sectionInset: UIEdgeInsets = .zero) -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = scrollDirection
+        layout.itemSize = itemSize
+        layout.minimumInteritemSpacing = interItemSpacing
+        layout.minimumLineSpacing = lineSpacing
+        layout.sectionInset = sectionInset
+        layout.invalidateLayout()
+        layout.footerReferenceSize = .zero
+        layout.headerReferenceSize = .zero
         return layout
     }
     
