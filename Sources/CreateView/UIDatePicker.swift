4864ba68545cd38db62f30e2de001d7788d23b61
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-09 15:08:45 +0300

    Add datePicker create extension

--- Sources/CreateView/UIDatePicker.swift
+++ Sources/CreateView/UIDatePicker.swift
@@ -12,8 +12,31 @@ public extension UIDatePicker {
     /**
      Create UIDatePicker.
      */
-    static func create() -> UIDatePicker {
+    static func create(mode: UIDatePicker.Mode = .dateAndTime,
+                       minuteInterval: Int = 0,
+                       locale: Locale = .current,
+                       timeZone: TimeZone = .current,
+                       date: Date? = nil,
+                       maxDate: Date? = nil,
+                       minDate: Date? = nil,
+                       countDownDuration: TimeInterval? = nil) -> UIDatePicker {
         let picker = UIDatePicker()
+        
+        picker.datePickerMode = mode
+        picker.minuteInterval = minuteInterval
+        picker.locale = locale
+        picker.timeZone = timeZone
+        picker.maximumDate = maxDate
+        picker.minimumDate = minDate
+        
+        if let date = date {
+            picker.date = date
+        }
+        
+        if let countDownDuration = countDownDuration {
+            picker.countDownDuration = countDownDuration
+        }
+        
         return picker
     }
 }
