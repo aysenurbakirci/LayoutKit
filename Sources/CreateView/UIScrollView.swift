549cf4300cbbda52ad50664a8ffaabb71918cfe7
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-09 23:25:24 +0300

    Add scroll view create extension

--- Sources/CreateView/UIScrollView.swift
+++ Sources/CreateView/UIScrollView.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
+// MARK: Create UIScrollView
 public extension UIScrollView {
     
     /**
      Create UIScrollView
      */
-    static func create() -> UIScrollView {
+    static func create(delegate: UIScrollViewDelegate,
+                       contentSize: CGSize = .zero,
+                       offset: CGPoint? = nil,
+                       inset: UIEdgeInsets? = nil,
+                       indicatorStyle: UIScrollView.IndicatorStyle = .default,
+                       isPagging: Bool = false,
+                       topScroll: Bool = false,
+                       bounces: Bool = false,
+                       showHorizontalIndicator: Bool = true,
+                       showVerticalIndicator: Bool = true,
+                       tamic: Bool = true) -> UIScrollView {
         let scroll = UIScrollView()
+        
+        scroll.delegate = delegate
+        scroll.contentSize = contentSize
+        
+        if let offset = offset {
+            scroll.contentOffset = offset
+        }
+        
+        if let inset = inset {
+            scroll.contentInset = inset
+        }
+        
+        scroll.indicatorStyle = indicatorStyle
+        scroll.isPagingEnabled = isPagging
+        scroll.scrollsToTop = topScroll
+        scroll.bounces = bounces
+        scroll.showsHorizontalScrollIndicator = showHorizontalIndicator
+        scroll.showsVerticalScrollIndicator = showVerticalIndicator
+        scroll.translatesAutoresizingMaskIntoConstraints = tamic
+        
         return scroll
     }
 }
