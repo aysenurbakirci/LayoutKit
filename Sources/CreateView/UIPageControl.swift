17e4615d9002e1ee9319c957669070a90e389d51
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-09 17:20:36 +0300

    Add page control create extension

--- Sources/CreateView/UIPageControl.swift
+++ Sources/CreateView/UIPageControl.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+// MARK: Create UIPageControl
 extension UIPageControl {
     
     /**
      Create UIPageControl
      */
-    static func create() -> UIPageControl {
+    static func create(currentPage: Int = 1,
+                       numberOfPages: Int,
+                       indicatorColor: UIColor = .gray,
+                       currentIndicatorColor: UIColor = .black,
+                       backgroundColor: UIColor = .clear,
+                       hidesForSinglePage: Bool = true,
+                       isOpaque: Bool = true,
+                       tamic: Bool = true) -> UIPageControl {
         let pageControl = UIPageControl()
+        
+        pageControl.currentPage = currentPage
+        pageControl.numberOfPages = numberOfPages
+        pageControl.pageIndicatorTintColor = indicatorColor
+        pageControl.currentPageIndicatorTintColor = currentIndicatorColor
+        pageControl.backgroundColor = backgroundColor
+        pageControl.hidesForSinglePage = hidesForSinglePage
+        pageControl.isOpaque = isOpaque
+        pageControl.translatesAutoresizingMaskIntoConstraints = tamic
         return pageControl
     }
 }
