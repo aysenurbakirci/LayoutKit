0f30589bc16d8de1482695d51dfbbdff7e4d41db
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-09 16:04:31 +0300

    Add image view create extension

--- Sources/CreateView/UIImageView.swift
+++ Sources/CreateView/UIImageView.swift
@@ -12,8 +12,25 @@ public extension UIImageView {
     /**
      Create UIImageView
      */
-    static func create() -> UIImageView {
+    static func create(image: UIImage? = nil,
+                       highlightedImage: UIImage? = nil,
+                       contentMode: ContentMode = .scaleToFill,
+                       isUserInteractionEnabled: Bool = false,
+                       backgroundColor: UIColor? = nil,
+                       tintColor: UIColor? = nil,
+                       clipsToBounds: Bool = false,
+                       isOpaque: Bool = true) -> UIImageView {
         let imageView = UIImageView()
+        
+        imageView.image = image
+        imageView.highlightedImage = highlightedImage
+        imageView.contentMode = contentMode
+        imageView.isUserInteractionEnabled = isUserInteractionEnabled
+        imageView.backgroundColor = backgroundColor
+        imageView.tintColor = tintColor
+        imageView.clipsToBounds = clipsToBounds
+        imageView.isOpaque = isOpaque
+        
         return imageView
     }
 }
