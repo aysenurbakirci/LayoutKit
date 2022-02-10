7dd629f2a173fcac2768463d33155222c771ae91
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-10 12:52:27 +0300

    Add stack view create extension

--- Sources/CreateView/UIStackView.swift
+++ Sources/CreateView/UIStackView.swift
@@ -12,8 +12,28 @@ public extension UIStackView {
     /**
      Create UIStackView
      */
-    static func create() -> UIStackView {
+    static func create(subviews: [UIView],
+                       axis: NSLayoutConstraint.Axis = .horizontal,
+                       distribution: UIStackView.Distribution = .fill,
+                       alignment: UIStackView.Alignment = .fill,
+                       spacing: CGFloat = 1.0,
+                       backgrounColor: UIColor = .clear,
+                       isBaselineRelativeArrangement: Bool = false,
+                       tamic: Bool = true) -> UIStackView {
         let stack = UIStackView()
+        
+        subviews.forEach { subview in
+            stack.addArrangedSubview(subview)
+        }
+        
+        stack.alignment = .center
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.spacing = spacing
+        stack.backgroundColor = backgrounColor
+        stack.isBaselineRelativeArrangement = isBaselineRelativeArrangement
+        stack.translatesAutoresizingMaskIntoConstraints = tamic
+        
         return stack
     }
 }
