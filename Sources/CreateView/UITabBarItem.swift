5973f0b723fcf53d9a8d3f6f8f132267624572fe
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-10 17:55:56 +0300

    Add tabBarItem create extension

--- Sources/CreateView/UITabBarItem.swift
+++ Sources/CreateView/UITabBarItem.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
+// MARK: Create UITabBarItem
 public extension UITabBarItem {
     
     /**
      Create UITabBarItem
      */
-    static func create() -> UITabBarItem {
-        let tabBarItem = UITabBarItem()
+    static func create(systemItem: UITabBarItem.SystemItem,
+                       tag: Int,
+                       badgeColor: UIColor = .black,
+                       badgeValue: String? = nil) -> UITabBarItem {
+        let tabBarItem = UITabBarItem(tabBarSystemItem: systemItem,
+                                      tag: tag)
+        tabBarItem.badgeColor = badgeColor
+        tabBarItem.badgeValue = badgeValue
+        
+        return tabBarItem
+    }
+    
+    static func create(title: String,
+                       image: UIImage,
+                       tag: Int,
+                       badgeColor: UIColor = .black,
+                       badgeValue: String? = nil) -> UITabBarItem {
+        let tabBarItem = UITabBarItem(title: title,
+                                      image: image,
+                                      tag: tag)
+        tabBarItem.badgeColor = badgeColor
+        tabBarItem.badgeValue = badgeValue
+        
+        return tabBarItem
+    }
+    
+    static func create(title: String,
+                       image: UIImage,
+                       selectedImage: UIImage,
+                       tag: Int,
+                       badgeColor: UIColor = .black,
+                       badgeValue: String? = nil) -> UITabBarItem {
+        let tabBarItem = UITabBarItem(title: title,
+                                      image: image,
+                                      selectedImage: selectedImage)
+        tabBarItem.tag = tag
+        tabBarItem.badgeColor = badgeColor
+        tabBarItem.badgeValue = badgeValue
+        
         return tabBarItem
     }
 }
