68ea072c31338b49ab6fc9a85bf7ff3bc8f31f00
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-10 12:37:12 +0300

    Add slider create extension

--- Sources/CreateView/UISlider.swift
+++ Sources/CreateView/UISlider.swift
@@ -12,8 +12,29 @@ public extension UISlider {
     /**
      Create UISlider
      */
-    static func create() -> UISlider {
+    static func create(currentValue: Float = 5.5,
+                       minValue: Float = 1.0,
+                       maxValue: Float = 10.0,
+                       minValueImage: UIImage? = nil,
+                       maxValueImage: UIImage? = nil,
+                       minTrackColor: UIColor = .black,
+                       maxTrackColor: UIColor = .gray,
+                       thumbColor: UIColor = .black,
+                       isContinuous: Bool = true,
+                       tamic: Bool = true) -> UISlider {
         let slider = UISlider()
+        
+        slider.value = currentValue
+        slider.minimumValue = minValue
+        slider.maximumValue = maxValue
+        slider.minimumValueImage = minValueImage
+        slider.maximumValueImage = maxValueImage
+        slider.minimumTrackTintColor = minTrackColor
+        slider.maximumTrackTintColor = maxTrackColor
+        slider.thumbTintColor = thumbColor
+        slider.isContinuous = isContinuous
+        slider.translatesAutoresizingMaskIntoConstraints = tamic
+        
         return slider
     }
 }
