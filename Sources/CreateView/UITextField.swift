c5791a3e8e8e128472e977d983c9bba705969b2d
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-10 18:11:02 +0300

    Add text field create extension

--- Sources/CreateView/UITextField.swift
+++ Sources/CreateView/UITextField.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+// MARK: Create UITextField
 public extension UITextField {
     
     /**
      Create UITextField
      */
-    static func create() -> UITextField {
+    static func create(delegate: UITextFieldDelegate,
+                       placeholder: String = "",
+                       textAlignment: NSTextAlignment = .right,
+                       font: UIFont = .systemFont(ofSize: 14.0),
+                       textColor: UIColor = .black,
+                       keyboardType: UIKeyboardType = .default) -> UITextField {
         let textField = UITextField()
+        
+        textField.delegate = delegate
+        textField.textAlignment = textAlignment
+        textField.font = font
+        textField.textColor = textColor
+        textField.placeholder = placeholder
+        textField.keyboardType = keyboardType
+        
         return textField
     }
 }
