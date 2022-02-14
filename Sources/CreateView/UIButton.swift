f122b676fc3b67216acf514038a6b80b8ecbcbfd
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-14 15:57:24 +0300

    Refactor UIButton create methods

--- Sources/CreateView/UIButton.swift
+++ Sources/CreateView/UIButton.swift
@@ -15,81 +15,27 @@ public extension UIButton {
      */
     static func create(type: ButtonType = .system,
                        title: String = "",
-                       titleColor: UIColor = .black,
-                       font: UIFont? = nil,
-                       numberOfLines: Int = 0,
-                       backgroundColor: UIColor = .clear,
-                       tamic: Bool = true,
-                       sizeToFit: Bool = true,
-                       horizontalAlignment: UIControl.ContentHorizontalAlignment = .center,
-                       verticalAlignment: UIControl.ContentVerticalAlignment = .center) -> UIButton {
-        let button = UIButton(type: type)
-        
-        button.setTitle(title, for: .normal)
-        button.setTitleColor(titleColor, for: .normal)
-        button.titleLabel?.numberOfLines = numberOfLines
-        button.titleLabel?.font = font
-        button.backgroundColor = backgroundColor
-        button.contentHorizontalAlignment = horizontalAlignment
-        button.contentVerticalAlignment = verticalAlignment
-        button.translatesAutoresizingMaskIntoConstraints = tamic
-        
-        if sizeToFit {
-            button.sizeToFit()
-        }
-        
-        return button
-    }
-    
-    /**
-     Create UIButton -> title and backround image.
-     */
-    static func create(type: ButtonType = .system,
-                       title: String = "",
-                       titleColor: UIColor = .black,
-                       font: UIFont? = nil,
-                       numberOfLines: Int = 0,
-                       backgroundImage: UIImage? = nil,
-                       tamic: Bool = true,
-                       sizeToFit: Bool = true,
-                       horizontalAlignment: UIControl.ContentHorizontalAlignment = .center,
-                       verticalAlignment: UIControl.ContentVerticalAlignment = .center) -> UIButton {
-        let button = UIButton(type: type)
-        
-        button.setTitle(title, for: .normal)
-        button.setTitleColor(titleColor, for: .normal)
-        button.setBackgroundImage(backgroundImage, for: .normal)
-        button.titleLabel?.numberOfLines = numberOfLines
-        button.titleLabel?.font = font
-        button.contentHorizontalAlignment = horizontalAlignment
-        button.contentVerticalAlignment = verticalAlignment
-        button.translatesAutoresizingMaskIntoConstraints = tamic
-        
-        if sizeToFit {
-            button.sizeToFit()
-        }
-        
-        return button
-    }
-    
-    /**
-     Create UIButton -> image and backround color.
-     */
-    static func create(type: ButtonType = .system,
-                       font: UIFont? = nil,
-                       numberOfLines: Int = 0,
                        image: UIImage? = nil,
-                       backgroundImage: UIImage? = nil,
+                       titleColor: UIColor = .black,
+                       font: UIFont? = nil,
+                       numberOfLines: Int = 0,
                        backgroundColor: UIColor = .clear,
+                       backgroundImage: UIImage? = nil,
                        tamic: Bool = true,
-                       sizeToFit: Bool = true) -> UIButton {
+                       sizeToFit: Bool = true,
+                       horizontalAlignment: UIControl.ContentHorizontalAlignment = .center,
+                       verticalAlignment: UIControl.ContentVerticalAlignment = .center) -> UIButton {
         let button = UIButton(type: type)
-    
+        
+        button.setTitle(title, for: .normal)
         button.setImage(image, for: .normal)
-        button.setBackgroundImage(backgroundImage, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
         button.titleLabel?.numberOfLines = numberOfLines
         button.titleLabel?.font = font
         button.backgroundColor = backgroundColor
+        button.setBackgroundImage(backgroundImage, for: .normal)
+        button.contentHorizontalAlignment = horizontalAlignment
+        button.contentVerticalAlignment = verticalAlignment
         button.translatesAutoresizingMaskIntoConstraints = tamic
         
         if sizeToFit {
